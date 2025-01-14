02ebbfd18cb6dd0f7f1ebceb09b22caaba70ea3d
tagnam <ravi@tagnamcommunications.com>
2025-01-14 13:12:53 +0530

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { testing_toolComponent } from './testing_tool/testing_tool.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    ProductComponent,
+    testing_toolComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { testing_toolComponent } from './testing_tool/testing_tool.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{ path:'', component:HomeComponent},
+{path:'about', component:AboutComponent},
+{path:'blog',component:BlogComponent},
+{ path:'contact',component:ContactComponent},
+{ path:'product', component:ProductComponent},
+{path:'tool',component:testing_toolComponent}
+
+];
